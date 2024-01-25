e4bf38dff95cb384fdb80a64c1059d32a8a07504
wayner0628 <a901639@gmail.com>
2024-01-25 16:31:01 +0800

    Initial commit

---
+++ Retriever/prepare_data.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import random
+import json
+
+channel = "train"
+
+output = []
+path = Path(f"../Datasets/FlawCheck/{channel}/review")
+sub = list(path.glob("*.json"))
+for pos in sub:
+    datapoint = {}
+    neg = random.choice(sub)
+    while neg == pos:
+        neg = random.choice(sub)
+    info = str(pos).replace("review", "info")
+    with open(info, "r") as file:
+        claim = json.load(file)
+    datapoint["question"] = claim["claim"]
+    datapoint["answers"] = []
+    datapoint["negative_ctxs"] = []
+    datapoint["positive_ctxs"] = []
+    datapoint["hard_negative_ctxs"] = []
+    with open(str(pos), "r") as file:
+        pos_review = json.load(file)
+    with open(str(neg), "r") as file:
+        neg_review = json.load(file)
+    start = False
+    text = ""
+    for j, content in enumerate(pos_review):
+        if "true" in content:
+            start = True
+        if "Our Sources" in content:
+            break
+        if start:
+            text += content
+    datapoint["positive_ctxs"].append({"title": "", "text": text})
+    start = False
+    text = ""
+    for j, content in enumerate(neg_review):
+        if "true" in content:
+            start = True
+        if "Our Sources" in content:
+            break
+        if start:
+            text += content
+    datapoint["hard_negative_ctxs"].append({"title": "", "text": text})
+    output.append(datapoint)
+with open(f"./data/{channel}.json", "w") as file:
+    json.dump(output, file)
---
+++ Retriever/retrieve.py
@@ -0,0 +1,105 @@
+from haystack.nodes import DensePassageRetriever
+from haystack.document_stores import InMemoryDocumentStore
+import concurrent.futures
+import json
+import pandas as pd
+from tqdm import tqdm
+import os
+
+retriever = DensePassageRetriever.load(load_dir="./models", document_store=None)
+
+
+def get_ruling(claim, review):
+    docs = []
+    for s in review:
+        docs.append({"content": s})
+    document_store = InMemoryDocumentStore()
+    document_store.write_documents(docs)
+    document_store.update_embeddings(retriever)
+    retriever.document_store = document_store
+    top_k_documents = retriever.retrieve(claim, top_k=30)
+
+    result = []
+    candi = []
+    for doc in top_k_documents:
+        if doc.score < 0.6:
+            break
+        candi.append(doc.content)
+    for s in review:
+        if s in candi:
+            result.append(s)
+    return result
+
+
+def get_evidence(claim, evidence):
+    docs = []
+    for s in evidence:
+        docs.append({"content": s})
+    document_store = InMemoryDocumentStore()
+    document_store.write_documents(docs)
+    document_store.update_embeddings(retriever)
+    retriever.document_store = document_store
+    top_k_documents = retriever.retrieve(claim, top_k=50)
+
+    candi = []
+    for doc in top_k_documents:
+        if doc.score < 0.6:
+            break
+        candi.append(doc.content)
+    result = []
+    for s in evidence:
+        if s in candi:
+            result.append(s)
+    return result
+
+
+# WatClaimCheck
+def run_thread_wat(x):
+    id = x["label"]["id"]
+    if not os.path.exists(f"../Datasets/WatClaimCheck/review/{id}.json"):
+        return
+    with open(f"../Datasets/WatClaimCheck/review/{id}.json", "r") as file:
+        review = json.load(file)
+    if review == []:
+        return
+
+    if input_split == "valid":
+        output_split = "dev"
+    else:
+        output_split = input_split
+
+    json.dump(
+        review, open(f"../Datasets/FlawCheck/{output_split}/review/wat_{id}.json", "w")
+    )
+    claim = x["metadata"]["claim"]
+    ruling = get_ruling(claim, review)
+    files = x["metadata"]["premise_articles"]
+    if output_split == "test":
+        evidence_pool = []
+        for article in files.values():
+            with open(f"../Datasets/WatClaimCheck/articles/{article}", "r") as file:
+                evidence = json.load(file)
+            evidence_pool += evidence
+        select_evidence = get_evidence(claim, evidence_pool)
+
+        with open(
+            f"../Datasets/FlawCheck/{output_split}/claim/wat_{id}.json", "w"
+        ) as file:
+            json.dump(claim, file)
+        with open(
+            f"../Datasets/FlawCheck/{output_split}/evidence/wat_{id}.json", "w"
+        ) as file:
+            json.dump(select_evidence, file)
+
+    with open(
+        f"../Datasets/FlawCheck/{output_split}/selected_review/wat_{id}.json", "w"
+    ) as file:
+        json.dump(ruling, file)
+
+
+for input_split in ["train", "valid", "test"]:
+    with open(f"../Datasets/WatClaimCheck/{input_split}.json", "r") as file:
+        data = json.load(file)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        tqdm(executor.map(run_thread_wat, data), total=len(data))
